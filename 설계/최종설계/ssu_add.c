54f3f479ed83504e93c425b1085a4d1ed28c5c09
배준형 <70988272+20190511@users.noreply.github.com>
2023-03-08 23:16:50 +0900

    Create ssu_add.c

---
+++ 설계/최종설계/ssu_add.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include "ssu_exec.h"
+
+extern char* optarg;
+
+int main(int argc, char* argv[])
+{
+    //ssu_add("/home/junhyeong/go", 1, 0);
+    int c;
+    int flag = 0;
+    char filename[MAXPATHLEN] = {0,};
+    char hash[5] = {0,};
+
+
+    strcpy(filename, argv[1]);
+    strcpy(hash, argv[argc-1]);
+    if (strcmp(hash, "md5") != 0 && strcmp(hash, "sha1") != 0)
+    {
+        printf("usage hash is <md5 | sha1>\n");
+        exit(1);
+    }
+
+    int flag_d = 0;
+    while((c = getopt(argc, argv, "d")) != -1)
+    {
+        switch (c)
+        {
+            case 'd':
+                flag_d = 1;
+                break;
+            case '?':
+                main_help();
+                break;
+
+            
+        }
+    }
+
+    if (access(filename, R_OK) != 0)
+    {
+        if (access(filename, F_OK) != 0)
+        {
+            printf("%s is not existed\n", filename);
+            exit(1);
+        }
+        else
+        {
+            printf("%s is can not access\n", filename);
+            exit(1);
+        }
+    }
+
+    int hash_num = (strcmp("md5", hash) == 0) ? 0 : 1;
+    if (flag_d)
+    {
+        if (argc != 4)
+        {
+            main_help();
+            exit(1);
+        }
+        printf("please wait .....\n");
+        /*
+        printf("option d\n");
+        printf("filename is %s\n", filename);
+        printf("hash is %s\n", hash);
+        */
+    }
+    else
+    {
+        if (argc != 3)
+        {
+            main_help();
+            exit(1);
+        }
+        /*
+        strcpy(filename,argv[1]);
+        printf("no option\n");
+        printf("filename is %s\n", filename);
+        printf("hash is %s\n", hash);
+        return 0;
+        */
+    }
+
+    ssu_add(filename, flag_d, hash_num);
+    exit(0);
+}
