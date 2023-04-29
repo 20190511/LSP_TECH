29cd393ed944f9a3dc69f42515db2217233c62b9
배준형 <70988272+20190511@users.noreply.github.com>
2023-04-29 16:36:32 +0900

    Update path.c

--- 설계2/path.c
+++ 설계2/path.c
@@ -1,64 +1,103 @@
-int ConvertPath(char* origin, char* resolved) {
-  int idx = 0;
-  int i;
-  char *path = (char *)malloc(sizeof(char *) * PATH_MAX);
-  char *tmppath = (char *)malloc(sizeof(char *) * PATH_MAX);
-  char **pathlist;
-  int pathcnt;
+typedef struct pn {
 
-  if(origin == NULL) {
-    return -1;
-  }
+    char path[MAXPATHLEN];
+    struct pn *prev;
+    struct pn *next;
+}pathNode;
+char** path_arr(char* str);
+int realpathS2 (char *str);
 
-  if(origin[0] == '~') {
-    sprintf(path, "%s%s", homePATH, origin+1);
-  } else if(origin[0] != '/') {
-    sprintf(path, "%s/%s", exePATH, origin);
-  } else {
-    sprintf(path, "%s", origin);
-  }
 
-  if(!strcmp(path, "/")) {
-    resolved = "/";
-    return 0;
-  }
-
-  if((pathlist = GetSubstring(path, &pathcnt, "/")) == NULL) {
-    return -1;
-  }
-
-  pathList *headpath = (pathList *)malloc(sizeof(pathList));
-  pathList *currpath = headpath;
-
-  for(i = 0; i < pathcnt; i++) {
-    if(!strcmp(pathlist[i], ".")) {
-      continue;
-    } else if(!strcmp(pathlist[i], "..")) {
-      currpath = currpath->prev;
-      currpath->next = NULL;
-      continue;
+int realpathS2 (char *str)
+{
+    if (str == NULL)
+        return 0;
+    int idx = 0;
+    char* home_path = getenv("HOME");
+    char cur_path[MAXPATHLEN];
+    getcwd(cur_path, MAXPATHLEN);
+    char *tmp_path = (char*)malloc(MAXPATHLEN);
+    char *path = (char*)malloc(MAXPATHLEN);
+     
+    strcpy(path, str);
+    if (path[0] == '~') {
+        snprintf(path, MAXPATHLEN, "%s/%s", home_path, str);
+    }
+    else if (path[0] != '/'){
+        snprintf(path, MAXPATHLEN+2, "%s/%s", cur_path, str);
     }
 
-    pathList *newpath = (pathList *)malloc(sizeof(pathList));
-    strcpy(newpath->path, pathlist[i]);
-    currpath->next = newpath;
-    newpath->prev = currpath;
+    char **lex = path_arr(str);
+    if (lex == NULL)
+        return 0;
 
-    currpath = currpath->next;
-  }
+    pathNode* head = (pathNode*)malloc(sizeof(pathNode));       //더미노드 생성.
+    pathNode* cur = head;
 
-  currpath = headpath->next;
+    //설계과제1 응용: 링크드리스트로 경로 받아서 . 면 for문 passing, .. 이면 pop 형태로 제작.
+    for (idx = 0 ; lex[idx] != NULL ; idx++)
+    {
+        if (!strcmp(lex[idx], "."))
+            continue;
+        else if (!strcmp(lex[idx], ".."))
+        {
+            cur = cur->prev;
+            cur->next = NULL;
+            continue;
+        }
 
-  strcpy(tmppath, "/");
-  while(currpath != NULL) {
-    strcat(tmppath, currpath->path);
-    if(currpath->next != NULL) {
-      strcat(tmppath, "/");
+        pathNode* newNode = (pathNode*)malloc(sizeof(pathNode));
+        strcpy(newNode->path, lex[idx]);
+        cur->next = newNode;
+        newNode->prev = cur;
+        cur = newNode;
     }
-    currpath = currpath->next;
-  }
+    
+    cur = head->next; //더미 노드이니까 하나 삭제.
+    while(cur != NULL)
+    {
+        printf("%s\n", cur->path);
+        cur = cur->next;
+    }
 
-  strcpy(resolved, tmppath);
+    return 1;
+}
 
-  return 0;
+
+char** path_arr(char* str)
+{
+    if (str == NULL)
+        return NULL;
+    
+    char* ptr = str;
+    char tok_path[MAXPATHLEN];
+    int tk_cnt = 0;
+    int i;
+    while (*ptr != '\0')
+    {        
+        if (*ptr == '/')
+            tk_cnt++;
+        ptr++;
+    }
+    if (str[0] == '/')
+        tk_cnt--;
+
+    if (tk_cnt == 0)
+        return NULL;
+    
+    char **lexeme_path = (char**)malloc(sizeof(char*) * (tk_cnt+1));
+    strcpy(tok_path, str);
+    
+    ptr = strtok(tok_path, "/");
+    for (i = 0 ; ptr != NULL ; i++)
+    {
+        if (ptr == NULL)
+            break;
+        
+        lexeme_path[i] = (char*)malloc(sizeof(tok_path));
+        strcpy(lexeme_path[i], ptr);
+        ptr = strtok(NULL, "/");
+    }
+    lexeme_path[i] = NULL;
+    return lexeme_path;
 }
