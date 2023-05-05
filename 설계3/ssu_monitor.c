993c5ba48d1647ca302fc756d205a6101cbc2b78
배준형 <70988272+20190511@users.noreply.github.com>
2023-05-05 23:13:19 +0900

    Update ssu_monitor.c

--- 설계3/ssu_monitor.c
+++ 설계3/ssu_monitor.c
@@ -8,6 +8,9 @@
 #include <wait.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/time.h>
+#include <time.h>
+#include <dirent.h>
 
 #define PROMPT() printf("20190511> ");
 
@@ -34,17 +37,65 @@ typedef struct pn {
 }pathNode;
 
 
+
+//디몬 프로세스 구조체 (이중연결리스트 형태)
+typedef struct lp {
+    char path [MAXPATHLEN];
+    
+    time_t mtime; // 최종 수정 시간
+    int visit;    // 생성 시 1, 디몬에서 monitoring 할 때 0으로 변경-->  다시확인할 때 0인지 확인!
+    
+    struct lp* prev;
+    struct lp* next;
+}LogPath;
+
+typedef struct lpl {
+    LogPath *head;
+    LogPath *tail;
+    int cnt;
+}LogpathList;
+
+
 char** path_arr(char* str); // str을 / 기준으로 토큰배열화
 int realpathS (char *str); // 링크드리스트기반 경로찾기 함수
 char** prom_args (char* str); // 프롬포트 토큰 배열 생성
 char* tokinzer (char* str, char* del); // del tokenizer
+int path_setting ();   // 현재경로, 디몬경로, log.txt 파일 경로를 알려주는 함수
+
+
+//** 디몬 프로세스꺼
+int make_time (time_t* t, char* buf); // 시간 출력
+LogPath* newL (char* path);
+LogpathList* newList (); // list 생성자
+/* path (디렉토리만 지원) 경로의 파일 모두 재귀호출하여 Regular File에 대해서 삽입*/
+LogpathList* scandir_list (LogpathList* list_arg, char* path, int depth); // list_arg 리스트 NULL 이면 새로 생성해서 return해줌
+LogPath* appendL (LogpathList *list, char* path);   // fl 
+LogPath* pop_path (LogpathList *list, char* path);  // list에서 path에 해당하는 노드 삭제
+LogPath* pop_node (LogpathList *list, LogPath* node); // list에서 node 즉시 삭제
+void printL (LogpathList *list);
+
+
+char current_path [MAXPATHLEN];
+char demon_path [MAXPATHLEN];
+char log_path [MAXPATHLEN];
+char* command_str [5] ={
+    "add",
+    "remove",
+    "help",
+    "exit",
+    "tree"
+};
+
 
 int main(void)
 {
     char prompt_str [PROMPTLEN];
     int i = 0;
     char** args;
-
+    if (!path_setting()) {
+        fprintf(stderr, "path setting error\n");
+        exit(1);
+    }
     while(1)
     {
         PROMPT();
@@ -63,10 +114,263 @@ int main(void)
         for (i = 0 ; args[i] != NULL ; i++)
             printf("%s\n", args[i]);
 
-    }
+        if (!strcmp(args[1], "search"))
+        {
+            char pathB [MAXPATHLEN];
+            strcpy(pathB, args[2]);
+            
+            if(!realpathS(pathB)){
+                fprintf(stderr, "path making error for %s\n", args[2]);
+                continue;
+            }
 
+            LogpathList* list = scandir_list(NULL, pathB, 0);
+            printL(list);
+        }
+
+    }
+    
     exit(0);
 
+}
+
+
+
+
+LogpathList* scandir_list (LogpathList* list_arg, char* path, int depth)
+{
+    DIR* dp;
+    struct dirent* dnt;
+    struct stat statbuf;
+    char tmp_path [MAXPATHLEN];
+    LogpathList* list;
+    int dir_cnt = 0;
+    int i;
+
+    setbuf(stdout, NULL);   
+    if (stat(path, &statbuf) < 0 ) {
+        fprintf(stderr, "path error for %s\n", path);
+        return NULL;
+    }
+
+    if (!S_ISDIR(statbuf.st_mode)) {
+        return NULL;
+    }
+
+    if (depth == 0 && list_arg == NULL)
+        list = newList();
+    else
+        list = list_arg;
+
+
+    strcpy(tmp_path, path);
+    char* ptr = tmp_path + strlen(tmp_path);
+    *ptr++ = '/';
+    *ptr = '\0';
+
+    if ((dp = opendir(tmp_path)) == NULL)
+    {
+        fprintf(stderr, "opendir error for %s\n", path);
+        return NULL;
+    }
+    
+    while((dnt = readdir(dp)) != NULL) {
+        if (strcmp(dnt->d_name, ".") && strcmp(dnt->d_name, ".."))
+        {
+            strcpy(ptr, dnt->d_name);
+
+            if (stat(tmp_path, &statbuf) < 0) 
+            {
+                fprintf(stderr, "stat error for %s\n", tmp_path);
+                return NULL;
+            }
+
+            if (S_ISDIR(statbuf.st_mode))
+            {
+                scandir_list(list, tmp_path, depth+1);
+            }
+            else if (S_ISREG(statbuf.st_mode))
+            {
+                appendL(list, tmp_path);
+            }
+       }
+    }
+    return list;
+}
+
+
+
+
+void printL (LogpathList *list)
+{
+    LogPath* tmp = list->head;
+    int cnt_idx;
+    for (cnt_idx = 0 ; tmp != NULL ; tmp = tmp->next, cnt_idx++)
+    {
+        char buf[30];
+        make_time(&tmp->mtime, buf);
+        printf("[%-4d] : %-70s   --> (%s)\n", cnt_idx+1, tmp->path, buf);
+    }
+}
+
+
+LogPath* newL (char* path)
+{
+    struct stat statbuf;
+    LogPath* newNode = (LogPath*)malloc(sizeof(LogPath));
+    strcpy(newNode->path, path);
+    newNode->next = newNode->prev = NULL;
+    newNode->visit = 1;
+
+    if (stat(path, &statbuf) < 0) {
+        fprintf (stderr, "stat error for %s\n", path);
+        free(newNode);
+        return NULL;
+    }
+
+    newNode->mtime = statbuf.st_mtime;
+    return newNode;
+}
+
+LogpathList* newList ()
+{
+    LogpathList* newNode = (LogpathList*)malloc(sizeof(LogpathList));
+    newNode->cnt = 0;
+    newNode->head = newNode->tail = NULL;
+    return newNode;
+}
+
+
+LogPath* appendL (LogpathList *list, char* path)
+{
+    if (list == NULL)
+        return NULL;
+    
+    LogPath* newNode = newL (path);
+    if (list->head == NULL)
+    {
+        list->head = list->tail = newNode;
+        list->cnt++;
+    }
+    else
+    {
+        list->tail->next = newNode;
+        newNode->prev = list->tail;
+        list->tail = newNode;
+        list->cnt++;
+    }   
+
+    return newNode;
+}
+
+LogPath* pop_path (LogpathList *list, char* path)
+{
+    if (list == NULL || list->cnt == 0)
+        return NULL;
+
+    LogPath* delNode = list->head;
+    
+    // 삭제 노드 찾기
+    while (delNode != NULL)
+    {
+        if (!strcmp(delNode->path, path))
+            break;
+        delNode = delNode->next;
+    }
+
+    if (delNode == NULL) {
+        printf("no path [%s] in list\n", path);
+        return NULL;
+    }
+
+    if (list->cnt == 1)
+    {
+        list->head = list->tail = NULL;
+    }
+    else if (delNode->prev == NULL) // 가장 첫 노드
+    {
+        list->head = delNode->next;
+        delNode->next->prev = NULL;
+    }
+    else if (delNode->next == NULL) // 가장 마지막 노드
+    {
+        list->tail = delNode->prev;
+        delNode->prev->next = NULL;
+    }
+    else
+    {
+        delNode->prev->next = delNode->next;
+        delNode->next->prev = delNode->prev;
+    }
+
+    list->cnt--;
+    delNode->prev = delNode->next = NULL;
+    return delNode;
+
+}
+
+
+LogPath* pop_node (LogpathList *list, LogPath* node)
+{
+    if (list == NULL || list->cnt == 0 || node == NULL)
+        return NULL;
+
+    LogPath* delNode = node;
+    
+    if (list->cnt == 1)
+    {
+        list->head = list->tail = NULL;
+    }
+    else if (delNode->prev == NULL) // 가장 첫 노드
+    {
+        list->head = delNode->next;
+        delNode->next->prev = NULL;
+    }
+    else if (delNode->next == NULL) // 가장 마지막 노드
+    {
+        list->tail = delNode->prev;
+        delNode->prev->next = NULL;
+    }
+    else
+    {
+        delNode->prev->next = delNode->next;
+        delNode->next->prev = delNode->prev;
+    }
+
+    list->cnt--;
+    delNode->prev = delNode->next = NULL;
+    return delNode;
+}
+
+
+int make_time (time_t* t, char* buf)
+{
+    if (t <= 0)
+        return false;
+
+    struct tm* tm;
+    if((tm = localtime(t)) == NULL) {
+        fprintf(stderr, "tm error\n");
+        return false;
+    }
+
+    sprintf(buf, "%d-%02d-%02d %02d:%02d:%02d", tm->tm_year+1900, tm->tm_mon+1, tm->tm_mday,
+    tm->tm_hour, tm->tm_min, tm->tm_sec);
+
+    return true;
+}
+
+
+int path_setting ()
+{
+    if (getcwd(current_path, MAXPATHLEN) < 0) {
+        fprintf(stderr, "getcwd error\n");
+        false;
+    }
+
+    snprintf(demon_path, MAXPATHLEN+10, "%s/daemon", current_path);
+    snprintf(log_path, MAXPATHLEN+10, "%s/log.txt", current_path);
+    return true;
 }
 
 
@@ -88,16 +392,16 @@ char** prom_args (char* str)
         arr_cnt++;
     }
     
-    token_arr = (char**)malloc(sizeof(char*) * (i+1));
-    
+    token_arr = (char**)malloc(sizeof(char*) * (i+3));
+    token_arr[0] = demon_path;
     char* tok = tokinzer(str, " ");
-    index = 0;
+    index = 1;
     while(tok != NULL)
     {
         token_arr[index++] = tok;
         tok = tokinzer(NULL, " ");
     }
-
+    token_arr[index++] = log_path;
     token_arr[index] = NULL;
     return token_arr;
 }
@@ -185,7 +489,7 @@ int realpathS (char *str)
      
     strcpy(path, str);
     if (path[0] == '~') {
-        sprintf(path, "%s/%s", home_path, str);
+        sprintf(path, "%s%s", home_path, str+1);
     }
     else if (path[0] != '/'){
         sprintf(path, "%s/%s", cur_path, str);
@@ -290,5 +594,5 @@ char** path_arr(char* str)
         ptr = strtok(NULL, "/");
     }
     lexeme_path[i] = NULL;
-    return lexeme_path;
+    return lexeme_path; 
 }
